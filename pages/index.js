cf29567f1a5dc8c6be5db650beaf0ea329a669b3
Mohd Razzeq <rzq272@gmail.com>
2023-02-25 03:51:19 +0800

    Remove redundant code

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ import p2image2 from "../public/project2/p2_bookdetail.png";
 import p2image3 from "../public/project2/p2_createbook.png";
 import p2image4 from "../public/project2/p2_profile.png";
 
-import Image from "next/image";
 import profileImage from "../public/profile.jpg";
 import { useState } from "react";
 import ScrollToTop from "react-scroll-to-top";
